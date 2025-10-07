16156abb1828adcec120d17ddfc2915fc5659773
VArmac <farrelnikoson@gmail.com>
2025-10-07 18:12:16 +0700

    fix

--- algoprog/16/tugas2a.c
+++ algoprog/16/tugas2a.c
@@ -1,4 +1,3 @@
-#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,37 +9,43 @@ typedef struct {
     char telepon[16];
     char jabatan[20];
     char golongan[2];
-    unsigned int gaji;
+    char gaji[10];
 } pegawai;
 
 int main() {
     pegawai pegawai;
-    printf("%-20s:", "Masukkan NIP");
+    printf("%-20s: ", "Masukkan NIP");
     scanf("%u", &pegawai.NIP);
-    printf("%-20s:", "Masukkan Nama");
+    printf("%-20s: ", "Masukkan Nama");
     scanf(" %[^\n]", pegawai.nama);
-    printf("%-20s:", "Masukkan Alamat");
+    printf("%-20s: ", "Masukkan Alamat");
     scanf(" %[^\n]", pegawai.alamat);
-    printf("%-20s:", "Masukkan No HP");
+    printf("%-20s: ", "Masukkan No HP");
     scanf("%s", pegawai.telepon);
-    printf("%-20s:", "Masukkan jabatan");
+    printf("%-20s: ", "Masukkan jabatan");
     scanf("%s", pegawai.jabatan);
-    printf("%-20s:", "Masukkan Golongan");
+    printf("%-20s: ", "Masukkan Golongan");
     scanf("%s", pegawai.golongan);
 
     if (strcmp(pegawai.golongan, "D1") == 0) {
-        pegawai.gaji = 3000000;
+        strcpy(pegawai.gaji, "3000000");
     } else if (strcmp(pegawai.golongan, "D2") == 0) {
-        pegawai.gaji = 2500000;
+        strcpy(pegawai.gaji, "2500000");
     } else if (strcmp(pegawai.golongan, "D3") == 0) {
-        pegawai.gaji = 2000000;
+        strcpy(pegawai.gaji, "2000000");
     } else {
-        printf("Runtime Error: masukkan golongan yang benar! (D1, D2, D3)");
+        printf("Runtime Error: masukkan golongan yang benar! (D1, D2, D3)\n");
         return EXIT_FAILURE;
     }
-    
-    setlocale(LC_NUMERIC, "id_ID.UTF-8");
 
-    printf("\nGaji Pokok Anda: Rp%.0f\n", (double)pegawai.gaji);
+    printf("\nGaji Pokok Anda: Rp");
+    int len = strlen(pegawai.gaji);
+    for(int i = 0; i < len; i++) {
+        printf("%c", pegawai.gaji[i]);
+        if ((i - len % 3 + 1) % 3 == 0 && i != len - 1) {
+            printf(".");
+        }
+    }
+    printf("\n");
     return EXIT_SUCCESS;
 }
