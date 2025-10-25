00d033b2ce8c21df4c40d0720eaf32799a05901e
VArmac <farrelnikoson@gmail.com>
2025-10-26 01:23:25 +0700

    cari.c

--- algoprog/22/cari.c
+++ algoprog/22/cari.c
@@ -1,1 +1,30 @@
-int main() {}
+typedef struct {
+    char nama[20];
+    int umur;
+    int noreg;
+} Siswa;
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+    FILE *fp = fopen("siswa.txt", "r");
+
+    char key[20];
+    _Bool found = 0;
+    printf("Cari siswa dengan nama: ");
+    scanf("%s", key);
+
+    Siswa siswa[10];
+    for (int i = 0; fscanf(fp, "%s %d %d", siswa[i].nama, &siswa[i].umur, &siswa[i].noreg) == 3; i++) {
+        Siswa *s = &siswa[i];
+        if(strcmp(key, s->nama) == 0) {
+            printf("Nama: %s\nUmur: %d\nNo registrasi: %d\n", s->nama, s->umur, s->noreg);
+            found = 1; break;
+        }
+    }
+    if (!found) {
+        printf("Data siswa dengan nama %s tidak ditemukan!\n", key);
+    }
+
+    fclose(fp);
+}
