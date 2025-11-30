ce655db1327b546c242befebbf214e7b1bf6fb60
VArmac <farrelnikoson@gmail.com>
2025-11-30 23:51:41 +0700

    oop design

---
+++ OOP/3/test.java
@@ -0,0 +1,39 @@
+class Email {
+    private final String signature;
+
+    public Email(String signature) {
+        this.signature = signature;
+    }
+
+    public String constructEmail(User recipient, String body) {
+        String str = "";
+        str += recipient.getNiceUserName();
+        str += "\n";
+        str += body;
+        str += "\n";
+        str += signature;
+        return str;
+    }
+}
+
+class User {
+    private String first;
+    private String last;
+
+    public User(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public String firstName() {
+        return first;
+    }
+
+    public String lastName() {
+        return last;
+    }
+
+    public String getNiceUserName() {
+        return firstName() + " " + lastName();
+    }
+}
